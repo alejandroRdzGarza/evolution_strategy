bdf5b235d4aaa48b4eb003f7f437e536e54a56f0
alejandroRdzGarza <maxhema17@gmail.com>
2024-03-19 16:46:38 -0600

    added visualizations

--- venv/EE/ee.py
+++ venv/EE/ee.py
@@ -12,9 +12,9 @@ def in_bounds(point, bounds):
 
 # Algoritmo (mu + lambda)-EE
 
-def ee_mas(funcion_objetivo, limites, iter_n, step_size, mu, lam, nombre_funcion):
+def ee_mas(funcion_objetivo, limites, iter_n, step_size, mu, lam, numero_funcion):
     mejor, mejor_eval = None, 1e+10
-    
+    history = []
     tam_pob, tam_sel = lam, mu
     
     n_hijos = int(tam_pob / tam_sel)
@@ -33,7 +33,7 @@ def ee_mas(funcion_objetivo, limites, iter_n, step_size, mu, lam, nombre_funcion
     #realizar la busqueda
     for epoch in range(iter_n):
         #evaluar aptitud de la poblacion
-        aptitudes = [funcion_objetivo(i, nombre_funcion) for i in poblacion]
+        aptitudes = [funcion_objetivo(i, numero_funcion) for i in poblacion]
         
         # rankear las aptitudes en orden ascendente
         ranking = np.argsort(np.argsort(aptitudes))
@@ -48,9 +48,9 @@ def ee_mas(funcion_objetivo, limites, iter_n, step_size, mu, lam, nombre_funcion
             if aptitudes[i] < mejor_eval:
                 mejor, mejor_eval = poblacion[i], aptitudes[i]
                 print("%d, Best: f(%s) = %.5f" % (epoch, mejor, mejor_eval))
-                
+                history.append(mejor)
                 #quedarse con el padre
-                hijos.append(poblacion[i])
+            hijos.append(poblacion[i])
                 
                 
                 
@@ -63,4 +63,5 @@ def ee_mas(funcion_objetivo, limites, iter_n, step_size, mu, lam, nombre_funcion
             
         #actualizar nueva generacion en la poblacion
         poblacion = hijos
-        return [mejor, mejor_eval]
+        
+    return [mejor, mejor_eval, history]
